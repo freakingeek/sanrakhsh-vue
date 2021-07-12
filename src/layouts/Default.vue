083216e7b45c7c9980dd5400026081118ce77e78
Yasin Silavi <yas.silavi28@gmail.com>
2021-07-12 14:05:27 +0430

    feat: add default layout

---
+++ src/layouts/Default.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="default-layout">
+    <TheHeader />
+    <slot />
+  </main>
+</template>
+
+<script>
+import TheHeader from '@/components/layouts/TheHeader'
+
+export default {
+  name: 'DefaultLayout',
+
+  components: {
+    TheHeader,
+  },
+}
+</script>
