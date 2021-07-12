2c4aa14128def00a0169d2c9fb82ccf121901854
Yasin Silavi <yas.silavi28@gmail.com>
2021-07-12 14:06:36 +0430

    feat: add washing powder product page

---
+++ src/pages/Products/WashingPowder/index.vue
@@ -0,0 +1,130 @@
+<template>
+  <PagesLayout>
+    <ProductsRow v-bind="firstSectionProps" />
+
+    <ProductsRow v-bind="secondSectionProps" reverce />
+  </PagesLayout>
+</template>
+
+<script>
+import ProductsRow from '@/components/ProductsRow'
+
+// Layout
+import PagesLayout from '@/layouts/Pages.vue';
+
+export default {
+  name: 'WashingPowder',
+
+  components: {
+    ProductsRow,
+
+    PagesLayout,
+  },
+
+  computed: {
+    firstSectionTitle() {
+      switch (this.$store.state.language) {
+        case 'en':
+          return 'Detergent powders'
+        case 'ar':
+          return 'مسحوق غسٌل آلً'
+        default:
+          return 'پودر شستشو ماشینی'
+      }
+    },
+
+    firstSectionText() {
+      switch (this.$store.state.language) {
+        case 'en':
+          return 'Detergent Powder san rakhsh is suitable for doing the laundry with a washing machine. It contains ingredients that completely wash the dirt off clothes and clean them. Detergent Powdersan rakhsh is an ideal option for removing stains from clothes'
+        case 'ar':
+          return 'ٌحتوي هذا المسحوق على مركبات تنظف المالبس وتنظفها تما ًما .مسحوق Sanrakhsh هو الخٌار المثالً لتلطٌخ المالبس.'
+        default:
+          return 'این پودر حاوی ترکیباتی است که لباس ها را کاملا پاک و تمیز میکند برای لکه زدایی لباس ها پودر سان رخش انتخابی ایده آل است.'
+      }
+    },
+
+    firstSectionProps() {
+      return {
+        title: this.firstSectionTitle,
+        description: this.firstSectionText,
+        products: [
+          {
+            name: 'نرم کننده اول',
+            description: 'بهترین نرم کننده جهان',
+            image: require('@/assets/images/products/01.jpeg'),
+          },
+          {
+            name: 'نرم کننده بزرگ',
+            description: 'بهترین نرم کننده جهان',
+            image: require('@/assets/images/products/02.jpeg'),
+          },
+        ],
+      }
+    },
+
+    secondSectionTitle() {
+      switch (this.$store.state.language) {
+        case 'en':
+          return 'Hand washing powder'
+        case 'ar':
+          return 'مسحوق غسٌل يدوي'
+        default:
+          return 'پودر شستشو دستی'
+      }
+    },
+
+    secondSectionText() {
+      switch (this.$store.state.language) {
+        case 'en':
+          return 'san rakhsh’s Hand Washing Detergent Powder is an excellent option for washing clothes by hand and with semiautomatic washing machines'
+        case 'ar':
+          return 'مسحوق سان رخش لغسل بالٌد خٌار ممتاز لغسل بالٌد وكذلك لالستخدام فً الغساالت شبه األوتوماتٌكٌة'
+        default:
+          return 'پودر سان رخش مخصوص شستشوی با دست گزینه ای بسیار عالی برای شستشوی دست و همچنین استفاده در ماشین لباسشویی نیمه اتوماتیک است'
+      }
+    },
+
+    secondSectionProps() {
+      return {
+        title: this.secondSectionTitle,
+        description: this.secondSectionText,
+        products: [
+          {
+            name: 'نرم کننده اول',
+            description: 'بهترین نرم کننده جهان',
+            image: require('@/assets/images/products/01.jpeg'),
+          },
+          {
+            name: 'نرم کننده بزرگ',
+            description: 'بهترین نرم کننده جهان',
+            image: require('@/assets/images/products/02.jpeg'),
+          },
+        ],
+      }
+    },
+  },
+
+  mounted() {
+    this.$store.commit('CHANGE_PAGE_TITLE', [
+      {
+        lang: 'fa',
+        title: 'پودر لباسشویی',
+        subTitle: 'لیست تمام پودرهای لباسشویی سان‌رخش',
+      },
+
+      {
+        lang: 'ar',
+        title: 'پودر لباسشویی',
+        subTitle: 'لیست تمام پودرهای لباسشویی سان‌رخش',
+      },
+
+      {
+        lang: 'en',
+        title: 'Washing Powder',
+        subTitle: 'List of all the sanrakhsh washing powder',
+      },
+    ])
+  },
+}
+</script>
