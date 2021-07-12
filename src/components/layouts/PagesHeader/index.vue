2a5b7ade9c26911fd314dfdad674ce97b78fe761
Yasin Silavi <yas.silavi28@gmail.com>
2021-07-12 14:07:37 +0430

    feat: add PagesHeader layout component

---
+++ src/components/layouts/PagesHeader/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="pages-header">
+    <h1>{{ title }}</h1>
+    <span>{{ subTitle }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PagesHeader',
+
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    subTitle: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style src="./pages-header.scss" lang="scss" scoped />
